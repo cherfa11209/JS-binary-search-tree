ece094adb468f3832f17b709e96c0fec0c4dcbcc
Sherif Cherfa <69639595+cherfa11209@users.noreply.github.com>
2021-02-21 17:13:17 +0000

    Create script.js
    I just started this project, I will be making a visualiser to go along with it to display the algorithms
    5%

---
+++ script.js
@@ -0,0 +1,23 @@
+class Node{
+    constructor(data, left, right){
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class BinarySearchTree{
+
+    constructor(root){
+        this.root = root
+    }
+
+    
+}
+
+
+let rootn = new Node(5, null, null) 
+
+let bst = new BinarySearchTree(rootn)
+
+console.log(bst)
